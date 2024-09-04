ad21bf3a330655e1a6a4d1d314cac8169ec87cfa
Adam Jelley <adjelley@gmail.com>
2024-09-04 15:56:18 +0100

    Project 10 (Compiler I) Implement Parser for single term expressions only.

--- 10/JackAnalyzer/CompilationEngine.py
+++ 10/JackAnalyzer/CompilationEngine.py
@@ -19,10 +19,6 @@ class CompilationEngine:
         self.write_indent()
         self.parsed_file.write(f'<{token_type}> {token} </{token_type}>')
 
-    def close_file(self):
-        self.parsed_file.write('</tokens>\n')
-        self.parsed_file.close()
-
     def compileClass(self):
         """
         Compile complete class (equivalent to file in Jack).
@@ -36,13 +32,19 @@ class CompilationEngine:
         # className
         self.write_token(token, token_type)
         token, token_type = self.tokenizer.advance()
-        # Opening bracket
+        # {
         self.write_token(token, token_type)
         while self.exists_classVarDec(self.tokenizer.view_next_token()):
             self.compileClassVarDec()
         while self.exists_subroutineDec(self.tokenizer.view_next_token()):
             self.compileSubroutine()
-            break
+        token, token_type = self.tokenizer.advance()
+        # }
+        self.write_token(token, token_type)
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write("</class>")
+        self.parsed_file.close()
 
     def compileClassVarDec(self):
         """
@@ -65,11 +67,10 @@ class CompilationEngine:
         while token != ';':
             self.write_token(token, token_type)
             token, token_type = self.tokenizer.advance()
-        else:
-            self.write_token(token, token_type)
-            self.decrement_indent()
-            self.write_indent()
-            self.parsed_file.write('</classVarDec>')
+        self.write_token(token, token_type)
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write('</classVarDec>')
 
     def compileSubroutine(self):
         """
@@ -94,6 +95,10 @@ class CompilationEngine:
         token, token_type = self.tokenizer.advance()
         # )
         self.write_token(token, token_type)
+        self.compileSubroutineBody()
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write("</subroutineDec>")
 
     def compileParameterList(self):
         self.write_indent()
@@ -107,28 +112,249 @@ class CompilationEngine:
         self.parsed_file.write('</parameterList>')
 
     def compileSubroutineBody(self):
-        pass
+        self.write_indent()
+        self.parsed_file.write("<subroutineBody>")
+        self.increment_indent()
+        token, token_type = self.tokenizer.advance()
+        # {
+        self.write_token(token, token_type)
+        while self.exists_varDec(self.tokenizer.view_next_token()):
+            self.compileVarDec()
+        self.compileStatements()
+        token, token_type = self.tokenizer.advance()
+        # }
+        self.write_token(token, token_type)
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write("</subroutineBody>")
 
     def compileVarDec(self):
-        pass
+        self.write_indent()
+        self.parsed_file.write("<varDec>")
+        self.increment_indent()
+        token, token_type = self.tokenizer.advance()
+        # var
+        self.write_token(token, token_type)
+        token, token_type = self.tokenizer.advance()
+        # type
+        self.write_token(token, token_type)
+        token, token_type = self.tokenizer.advance()
+        # varName
+        self.write_token(token, token_type)
+        token, token_type = self.tokenizer.advance()
+        # Multiple variables of same type
+        while token != ";":
+            self.write_token(token, token_type)
+            token, token_type = self.tokenizer.advance()
+        # ;
+        self.write_token(token, token_type)
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write('</varDec>')
 
     def compileStatements(self):
-        pass
+        self.write_indent()
+        self.parsed_file.write("<statements>")
+        self.increment_indent()
+        next_token = self.tokenizer.view_next_token()
+        while next_token != "}":
+            if next_token == 'let':
+                self.compileLet()
+            elif next_token == "if":
+                self.compileIf()
+            elif next_token == "while":
+                self.compileWhile()
+            elif next_token == "do":
+                self.compileDo()
+            elif next_token == "return":
+                self.compileReturn()
+            else:
+                raise ValueError(
+                    f"Token {next_token} is not a known statement type, nor closing brace."
+                )
+            next_token = self.tokenizer.view_next_token()
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write("</statements>")
 
     def compileLet(self):
-        pass
+        self.write_indent()
+        self.parsed_file.write("<letStatement>")
+        self.increment_indent()
+        token, token_type = self.tokenizer.advance()
+        # let
+        self.write_token(token, token_type)
+        token, token_type = self.tokenizer.advance()
+        # varName
+        self.write_token(token, token_type)
+        token, token_type = self.tokenizer.advance()
+        if token == '[':
+            self.write_token(token, token_type)
+            self.compileExpression()
+            token, token_type = self.tokenizer.advance()
+            # ]
+            self.write_token(token, token_type)
+            token, token_type = self.tokenizer.advance()
+        # =
+        self.write_token(token, token_type)
+        self.compileExpression()
+        token, token_type = self.tokenizer.advance()
+        # ;
+        self.write_token(token, token_type)
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write("</letStatement>")
 
     def compileIf(self):
-        pass
+        self.write_indent()
+        self.parsed_file.write("<ifStatement>")
+        self.increment_indent()
+        token, token_type = self.tokenizer.advance()
+        # if
+        self.write_token(token, token_type)
+        token, token_type = self.tokenizer.advance()
+        # (
+        self.write_token(token, token_type)
+        self.compileExpression()
+        token, token_type = self.tokenizer.advance()
+        # )
+        self.write_token(token, token_type)
+        token, token_type = self.tokenizer.advance()
+        # {
+        self.write_token(token, token_type)
+        self.compileStatements()
+        token, token_type = self.tokenizer.advance()
+        # }
+        self.write_token(token, token_type)
+        next_token = self.tokenizer.view_next_token()
+        if next_token == 'else':
+            token, token_type = self.tokenizer.advance()
+            self.write_token(token, token_type)
+            token, token_type = self.tokenizer.advance()
+            # {
+            self.write_token(token, token_type)
+            self.compileStatements()
+            token, token_type = self.tokenizer.advance()
+            # }
+            self.write_token(token, token_type)
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write("</ifStatement>")
 
     def compileWhile(self):
-        pass
+        self.write_indent()
+        self.parsed_file.write("<whileStatement>")
+        self.increment_indent()
+        token, token_type = self.tokenizer.advance()
+        # while
+        self.write_token(token, token_type)
+        token, token_type = self.tokenizer.advance()
+        # (
+        self.write_token(token, token_type)
+        self.compileExpression()
+        token, token_type = self.tokenizer.advance()
+        # )
+        self.write_token(token, token_type)
+        token, token_type = self.tokenizer.advance()
+        # {
+        self.write_token(token, token_type)
+        self.compileStatements()
+        token, token_type = self.tokenizer.advance()
+        # }
+        self.write_token(token, token_type)
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write("</whileStatement>")
 
     def compileDo(self):
-        pass
+        self.write_indent()
+        self.parsed_file.write("<doStatement>")
+        self.increment_indent()
+        token, token_type = self.tokenizer.advance()
+        # do
+        self.write_token(token, token_type)
+        self.compileSubroutineCall()
+        token, token_type = self.tokenizer.advance()
+        # ;
+        self.write_token(token, token_type)
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write("</doStatement>")
 
     def compileReturn(self):
-        pass
+        self.write_indent()
+        self.parsed_file.write("<returnStatement>")
+        self.increment_indent()
+        token, token_type = self.tokenizer.advance()
+        # return
+        self.write_token(token, token_type)
+        if self.tokenizer.view_next_token() != ';':
+            self.compileExpression()
+        token, token_type = self.tokenizer.advance()
+        # ;
+        self.write_token(token, token_type)
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write("</returnStatement>")
+
+    def compileExpression(self):
+        self.write_indent()
+        self.parsed_file.write("<expression>")
+        self.increment_indent()
+
+        self.compileTerm()
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write('</expression>')
+
+    def compileTerm(self):
+        self.write_indent()
+        self.parsed_file.write("<term>")
+        self.increment_indent()
+        token, token_type = self.tokenizer.advance()
+        # Assume term is constant for now.
+        self.write_token(token, token_type)
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write('</term>')
+
+    def compileSubroutineCall(self):
+        """
+        Compile subroutine call.
+        Note: Ignoring advice from lectures to include subroutine calls as if they were expressions, since subroutine calls do not appear to include <expression> and <term> tags in provided xml files.
+        """
+        token, token_type = self.tokenizer.advance()
+        # subroutineName | className | varName
+        self.write_token(token, token_type)
+        token, token_type = self.tokenizer.advance()
+        if token == '.':
+            self.write_token(token, token_type)
+            token, token_type = self.tokenizer.advance()
+            # subroutineName
+            self.write_token(token, token_type)
+            token, token_type = self.tokenizer.advance()
+        # (
+        self.write_token(token, token_type)
+        self.compileExpressionList()
+        token, token_type = self.tokenizer.advance()
+        # )
+        self.write_token(token, token_type)
+
+    def compileExpressionList(self):
+        self.write_indent()
+        self.parsed_file.write("<expressionList>")
+        self.increment_indent()
+        next_token = self.tokenizer.view_next_token()
+        while next_token != ')':
+            self.compileExpression()
+            next_token = self.tokenizer.view_next_token()
+            if next_token == ',':
+                token, token_type = self.tokenizer.advance()
+                self.write_token(token, token_type)
+                next_token = self.tokenizer.view_next_token()
+        self.decrement_indent()
+        self.write_indent()
+        self.parsed_file.write("</expressionList>")
 
     def exists_classVarDec(self, token):
         if token == 'static' or token == 'field':
@@ -138,6 +364,12 @@ class CompilationEngine:
 
     def exists_subroutineDec(self, token):
         if token == 'constructor' or token == 'function' or token == 'method':
+            return True
+        else:
+            return False
+
+    def exists_varDec(self, token):
+        if token == 'var':
             return True
         else:
             return False
